84ad97aca867efb0806a06afd5197278cdfa02de
Dylain Cohen <dylain.cohen@wework.com>
2018-02-18 00:41:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,9 @@
 
-board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
 
 
 def turn_count(board)
-  counter = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      counter +=
-    end
-  end
+
 end
 
 
