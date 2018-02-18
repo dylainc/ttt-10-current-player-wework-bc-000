5ea815bb3f10f5bbf9ef3ed2d8ce0f169a6c712a
Dylain Cohen <dylain.cohen@wework.com>
2018-02-18 00:43:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,12 +5,10 @@
 def turn_count(board)
   counter = 0
   board.each do |token|
-    if token == "X" || token == "O"
-      counter +=
-    end
+
   end
   counter
-
+end
 
 
 def current_player(board)
